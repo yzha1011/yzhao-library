14b87c3f9b95fe4cbfc6baa9d4c6faba2253b854
yzha1011 <yzha1011@student.monash.edu>
2024-08-02 18:51:23 +1000

    first push, complete all the task

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue';
+</script>
+
+<template>
+  <!-- <header> -->
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
+  <div class="box">
+    <JSON />
+  </div>
+  
+  <!-- </header> -->
+
+  <!-- <main> -->
+    <!-- <TheWelcome /> -->
+    
+  <!-- </main> -->
+</template>
+
+<style scoped>
+
+.box {
+  width: 1152px;
+  margin: 0 auto;
+
+}
+
+
+ 
+</style>
