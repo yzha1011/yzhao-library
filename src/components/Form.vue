9007729e394d13dc4ade3070388f1940284d8b97
yzha1011 <yzha1011@student.monash.edu>
2024-08-12 13:51:06 +1000

    complete the week2 task7

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -29,24 +29,24 @@
       <div class="col-md-8 offset-md-2">
         <h1 class="text-center">User Information Form</h1>
         <form @submit.prevent="submitForm">
-          <div class="row mb-3">
-            <div class="col-md-6">
+          <div class="row sm-3">
+            <div class="col-sm-6">
               <label for="username" class="form-label">Username</label>
               <input type="text" class="form-control" id="username" v-model="formData.username">
             </div>
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <label for="password" class="form-label">Password</label>
               <input type="password" class="form-control" id="password" v-model="formData.password">
             </div>
           </div>
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian">
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <label for="gender" class="form-label">Gender</label>
               <select class="form-select" id="gender" v-model="formData.gender">
                 <option value="male">Male</option>
